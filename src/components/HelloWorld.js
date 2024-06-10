a8c5ffee3fc71217470cfca23c2242c5350754c8
vchinnap <105466123+vchinnap@users.noreply.github.com>
2024-06-10 19:34:09 -0400

    Update HelloWorld.js

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to your first React app deployed with AWS Amplify.</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
 
