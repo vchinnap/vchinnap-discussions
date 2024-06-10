44fb12f21ae92a32f5e8151f6c931904b5b68a54
vchinnap <105466123+vchinnap@users.noreply.github.com>
2024-06-10 19:34:24 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HelloWorld />
+      </header>
+    </div>
+  );
+}
+
+export default App;
 
