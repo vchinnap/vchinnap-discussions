f20953ff3e3c02d7fe8762f7ddf24cb4d2116c95
vchinnap <105466123+vchinnap@users.noreply.github.com>
2025-01-14 10:50:20 -0500

    Update insights.sql

--- src/insights.sql
+++ src/insights.sql
@@ -5,6 +5,12 @@ fields @timestamp, @message
 | display resource_name, status, job_id, resource_type, error_message
 | limit 50
 
+fields @timestamp, @message
+| parse @message /Account ID: (?<account_id>[^,]+), Resource Name: (?<resource_name>[^,]+), Status: (?<status>[^,]+), Job ID: (?<job_id>[^,]+), Resource Type: (?<resource_type>[^,]+), Message: (?<error_message>.+)/
+| filter status = "FAILED" and resource_type = "RDS"
+| sort @timestamp desc
+| display account_id, resource_name, status, job_id, resource_type, error_message
+| limit 50
 
 
 fields @timestamp, @message
