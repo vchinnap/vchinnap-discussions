17f6e6aff197cdd151f433cf7c9c1939277edab9
vchinnap <105466123+vchinnap@users.noreply.github.com>
2025-02-25 17:06:18 -0500

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -38,3 +38,58 @@ class AzureVmControlStack extends TerraformStack {
 const app = new App();
 new AzureVmControlStack(app, 'azure-vm-control');
 app.synth();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import * as cdk from 'aws-cdk-lib';
+import { BBBBAWSConfigRuleConstruct } from '@bbbb-cdk/aws-config';
+import { Construct } from 'constructs';
+import * as config from 'aws-cdk-lib/aws-config';
+
+export class EbsOptimizedInstanceRule extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // AWS Config Rule
+        const configRule = new BBBBAWSConfigRuleConstruct(this, 'aws-config-rule', {
+            source: {
+                owner: 'AWS',
+                sourceIdentifier: 'EBS_OPTIMIZED_INSTANCE',
+            },
+            configRuleName: 'hwss-ebs-optimized-instance',
+            description: 'Checks if Amazon EBS optimization is enabled for EC2 instances.',
+        });
+
+        // Dummy Remediation using SNS Notification
+        new config.CfnRemediationConfiguration(this, 'EbsOptimizationSNSRemediation', {
+            configRuleName: configRule.configRuleName,  // Attach to the existing rule
+            targetId: "arn:aws:sns:<REGION>:<ACCOUNT_ID>:TestRemediationSNS", // SNS Topic
+            targetType: "SNS",
+            parameters: {
+                "Message": {
+                    StaticValue: {
+                        Values: ["AWS Config detected non-compliance: EBS optimization is not enabled."]
+                    }
+                },
+                "TopicArn": {
+                    StaticValue: {
+                        Values: ["arn:aws:sns:<REGION>:<ACCOUNT_ID>:TestRemediationSNS"]
+                    }
+                }
+            },
+            automatic: true // Automatically trigger SNS when non-compliant
+        });
+    }
+}
