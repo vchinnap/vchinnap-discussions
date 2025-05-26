6557f63c1a7dea7a9008297f1df764ff56591269
vchinnap <105466123+vchinnap@users.noreply.github.com>
2025-05-26 18:45:33 -0400

    Update tags.py

--- src/config/tags.py
+++ src/config/tags.py
@@ -16,3 +16,30 @@ def lambda_handler(event, context):
     return {
         'PhysicalResourceId': f'{arn}-TagApplied'
     }
+
+
+
+import boto3
+import os
+
+def lambda_handler(event, context):
+    config = boto3.client('config')
+    rule_arn = os.environ['CONFIG_RULE_ARN']
+
+    tags = [
+        {'Key': key.replace('TAG_', ''), 'Value': value}
+        for key, value in os.environ.items()
+        if key.startswith('TAG_')
+    ]
+
+    print(f"🧾 Tagging rule {rule_arn} with tags: {tags}")
+
+    if event['RequestType'] == 'Create':
+        config.tag_resource(
+            ResourceArn=rule_arn,
+            Tags=tags
+        )
+
+    return {
+        'PhysicalResourceId': f'{rule_arn}-TagApplied'
+    }
