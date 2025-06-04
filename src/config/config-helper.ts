7a8374451f93cefb8ecb1af2b645702ab2deb247
vchinnap <105466123+vchinnap@users.noreply.github.com>
2025-06-04 14:53:01 -0400

    commmit

--- src/config/config-helper.ts
+++ src/config/config-helper.ts
@@ -121,12 +121,19 @@ export class ConfigRuleWithRemediationConstruct extends Construct {
       const customRule = new config.CustomRule(this, `${ruleName}-ConfigRule`, {
         configRuleName: ruleName,
         description,
-        periodic: isPeriodic ?? true,
-        maximumExecutionFrequency,
+        ...(isPeriodic !== undefined || maximumExecutionFrequency !== undefined
+        ? {
+            periodic: isPeriodic ?? true,
+            maximumExecutionFrequency: maximumExecutionFrequency ?? config.MaximumExecutionFrequency.TWENTY_FOUR_HOURS
+          }
+        : {
+            periodic: false
+        }),
         lambdaFunction: this.evaluationLambda.lambdaFunction,
         ruleScope
       });
       configRuleArn = customRule.configRuleArn;
+
 
       const taggingLambda = new MBOLambdaConstruct(this, `${ruleName}-ConfigTagLambda`, {
         functionName: functionRuleName,
